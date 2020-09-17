de5b16e32091edd8b4b95c3b00922f0249caf61f
mi-os <jbhjmy@yahoo.co.jp>
2020-09-17 22:15:27 +0900

    add link to home

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (
+        <>
+        <h1>404 - Page Not Found</h1>
+        <p>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+        </p>
+        </>
+    )
 }
