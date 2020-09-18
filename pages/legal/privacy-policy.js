07d3a3f0f2dde2e9a42b20f44b836f21bbd00a00
mi-os <jbhjmy@yahoo.co.jp>
2020-09-19 06:25:05 +0900

    initial commit

---
+++ pages/legal/privacy-policy.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+export default function PrivacyPolicy() {
+    return (
+        <Layout>
+            <Head>
+                <title>Privacy Policy</title>
+            </Head>
+            <h1>Privacy Policy</h1>
+            <p>Welcome to My Next.js test app site.</p>
+        </Layout>
+    )
+}
